e0c63d02676563adcc7350d8c5268f39919da9b0
Walker <walker.xing@assetech.com>
2025-09-10 15:48:44 +0800

    修改测试3

--- js/customer_site.js
+++ js/customer_site.js
@@ -1,7 +1,7 @@
 const CUSTOMER_SITES = {
     qiqi: {
-        api: 'https://cj.rycjapi.com/api.php/provide/vod\'',
-        name: '如意',
+        api: 'https://cj.rycjapi.com/api.php/provide/vod',
+        name: '八八',
     }
 };
 
