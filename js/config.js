e0bc957bc5c900e8979dc6eec6747c2409be4af5
Walker <walker.xing@assetech.com>
2025-09-16 08:41:02 +0800

    修改测试3

--- js/config.js
+++ js/config.js
@@ -22,17 +22,10 @@ const SITE_CONFIG = {
 
 // API站点配置
 const API_SITES = {
-    testSource: {
-        api: 'https://cj.rycjapi.com/api.php/provide/vod',
-        name: '如意资源'
-    } ,   dyttzy: {
-
+ dyttzy: {
         api: 'http://caiji.dyttzyapi.com/api.php/provide/vod',
-
         name: '电影天堂资源',
-
         detail: 'http://caiji.dyttzyapi.com',
-
     },
 
     ruyi: {
